83cad434c34e12223b58a5cac3a57ff64e752caa
Erlend Svanevik <erlendms@hotmail.no>
2026-05-11 22:50:29 +0200

    chore: Update socials.json, package.json

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "portfolio", // ? for GitHub pages
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
