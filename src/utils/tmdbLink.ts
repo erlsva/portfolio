ef696cada0c7ac90560d3559f67054c15286afbc
Erlend Svanevik <erlendms@hotmail.no>
2026-07-20 02:05:34 +0200

    chore: removed unused files

--- src/utils/tmdbLink.ts
+++
@@ -1,7 +0,0 @@
-export const tmdbLink = (
-  id: number,
-  name: string,
-  type: "movie" | "tv",
-): string => {
-  return `https://www.themoviedb.org/${type}/${id}-${name.replaceAll(" ", "-")}`;
-};
